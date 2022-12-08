1aaa56e9c6d1e0d207073fa0c74ab41db6d02140
jakub-mrow <jakub@mrowczynski.net>
2022-12-08 21:56:15 +0100

    Example implementation of nested models and serializations with reservations, guests, rooms and employees

--- server/src/skiresort/serializers.py
+++ server/src/skiresort/serializers.py
@@ -10,10 +10,54 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class RoomSerializer(serializers.ModelSerializer):
-    description = serializers.CharField(help_text="description")
-    beds = serializers.IntegerField()
-    price = serializers.IntegerField()
+    room_id = serializers.IntegerField()
+    wing = serializers.CharField(required=False)
+    description = serializers.CharField(required=False, help_text="description")
+    beds = serializers.IntegerField(required=False)
+    price = serializers.IntegerField(required=False)
 
     class Meta:
         model = models.Room
-        fields = '__all__'
+        fields = "__all__"
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    social_security_number = serializers.IntegerField(help_text="Polish social security number PESEL")
+    surname = serializers.CharField(required=False, max_length=128)
+    name = serializers.CharField(required=False, max_length=128)
+    job = serializers.CharField(required=False, max_length=128)
+    salary = serializers.IntegerField(required=False)
+
+    class Meta:
+        model = models.Employee
+        fields = "__all__"
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+    #room = RoomSerializer()
+    date_from = serializers.CharField(required=False, max_length=128)
+    date_to = serializers.CharField(required=False, max_length=128)
+    number_of_people = serializers.IntegerField(required=False)
+    #employee = EmployeeSerializer()
+
+    class Meta:
+        model = models.Reservation
+        fields = "__all__"
+
+    def create(self, validated_data):
+        reservation = models.Reservation.objects.create(**validated_data)
+
+        return reservation
+
+
+class GuestSerializer(serializers.ModelSerializer):
+    social_security_number = serializers.CharField(required=False)
+    surname = serializers.CharField(required=False)
+    name = serializers.CharField(required=False)
+    email = serializers.EmailField(required=False)
+    address = serializers.CharField(required=False)
+    reservations = ReservationSerializer(many=True, required=False)
+
+    class Meta:
+        model = models.Guest
+        fields = "__all__"
